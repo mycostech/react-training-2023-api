eb693a88359a2000138ce82414c5165d27cf0fba
sooparerk <sooparerk@mycostech.com>
2024-08-28 15:53:15 +0700

    Add connection count

--- ReactTraining2023/Hubs/ScoreHub.cs
+++ ReactTraining2023/Hubs/ScoreHub.cs
@@ -7,10 +7,34 @@ namespace ReactTraining2023.Hubs
     public class ScoreHub : Hub
     {
         public static readonly ConcurrentDictionary<string, ConcurrentDictionary<string, int>> _appScores = new ConcurrentDictionary<string, ConcurrentDictionary<string, int>>();
+        private static int _connectionCount = 0;
 
+        // Method called when a client connects
+        public override Task OnConnectedAsync()
+        {
+            // Increment the connection count
+            _connectionCount++;
+
+            // Notify all clients about the updated count
+            Clients.All.SendAsync("UpdateConnectionCount", _connectionCount);
+
+            return base.OnConnectedAsync();
+        }
+
+        // Method called when a client disconnects
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            // Decrement the connection count
+            _connectionCount--;
+
+            // Notify all clients about the updated count
+            Clients.All.SendAsync("UpdateConnectionCount", _connectionCount);
+
+            return base.OnDisconnectedAsync(exception);
+        }
         public async Task CreateApp(string appName)
         {
-             _appScores.GetOrAdd(appName, new ConcurrentDictionary<string, int>());
+            _appScores.GetOrAdd(appName, new ConcurrentDictionary<string, int>());
             await Groups.AddToGroupAsync(Context.ConnectionId, appName);
         }
 
